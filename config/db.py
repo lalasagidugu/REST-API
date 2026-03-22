e5d7b988a6e91fccc2a1f12ba29302259ac0b309
Lalasa <lalasagidugu55@gmail.com>
2026-03-22 15:45:41 +0530

    Updated database connection to use .env and remote DB

--- config/db.py
+++ config/db.py
@@ -1,15 +1,19 @@
-import os
 from sqlalchemy import create_engine, MetaData
 from urllib.parse import quote_plus
-from dotenv import load_dotenv
+import os
 
-load_dotenv()  
 
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD", "lalasa@123"))
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "restapi_db")
 
-engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}", pool_pre_ping=True)
+
+engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
 meta = MetaData()
-conn = engine.connect()
+
+try:
+    conn = engine.connect()
+    print("Database connected!")
+except Exception as e:
+    print("Database connection failed:", e)
